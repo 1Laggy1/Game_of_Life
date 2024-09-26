9401f36b46c3722ad86ec336363766f8e4d82e63
1_laggy_1 <andryushka05@gmail.com>
2024-09-26 11:22:02 +0300

    Cell logic && NUnit
    - added simple cell logic
    - added NUnit package

--- Program.cs
+++ Program.cs
@@ -1,7 +1,28 @@
-﻿internal class Program
+﻿using GameOfLife;
+
+internal class Program
 {
-    private static void Main(string[] args)
+    public Cell[][] Сells = new Cell[8][];
+    private void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        Сells = InitializeCells();
     }
+    public Cell[][] InitializeCells()
+    {
+        Cell[][] cells = new Cell[8][];
+
+        for (int i = 0; i < cells.Length; i++)
+        {
+            cells[i] = new Cell[5];
+
+            for (int j = 0; j < cells[i].Length; j++)
+            {
+                cells[i][j] = new Cell();
+            }
+        }
+
+        return cells;
+    }
+
 }
